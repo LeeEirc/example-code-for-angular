445c3bcc7bb861bcd36df5bed44bc77bb1feaa6a
Eric <xplzv@126.com>
2018-05-11 17:53:44 +0800

    update to chapter 5 code

---
+++ src/app/metric/metric.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  templateUrl: './metric.component.html',
+  styleUrls: ['./metric.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MetricComponent implements OnInit, OnChanges {
+  private _value: number = 0;
+  private _max: number = 100;
+
+  @Input('used')
+  set value(value: number) {
+    if (isNaN(value)) {
+      value = 0;
+    }
+    this._value = value;
+
+  }
+
+  get value(): number { return this._value; }
+
+  @Input('available')
+  set max(max: number) {
+    if (isNaN(max)) {
+      max = 100;
+    }
+    this._max = max;
+  }
+
+  get max(): number {
+    return this._max;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes) {
+    console.log(changes);
+    if (changes.value && isNaN(changes.value.currentValue)) {
+      this.value = 0;
+    }
+    if (changes.max && isNaN(changes.max.currentValue)) {
+      this.max = 0;
+    }
+  }
+
+  isDanger() {
+    return this.value / this.max > 0.7;
+  }
+}
---
+++ src/app/nodes-row/nodes-row.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: '[app-nodes-row]',
+  templateUrl: './nodes-row.component.html',
+  styleUrls: ['./nodes-row.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NodesRowComponent implements OnInit {
+  @Input() node: any;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isDanger(prop) {
+    return this.node[prop].used / this.node[prop].available > 0.7;
+  }
+
+}
